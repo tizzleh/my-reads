cdf7899c27fae9ec87677883b75df18cf87375cd
Ty Harlacker <harlacker@gmail.com>
2018-09-08 01:07:01 -0600

    feat: Add prop-types package

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
 class Shelf extends Component {
 
     render() {
         const shelfChange = this.props
         const books = this.props
-
+        // TODO: Add proptypes
         return (
             <ol className="books-grid">
                 {books.map((book) =>(
